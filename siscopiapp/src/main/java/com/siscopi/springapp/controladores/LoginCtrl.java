34678f7fff4320e57e3712dd6c4801b5db4c3e42
Maricela Santiago Cayetano <mary171287@gmail.com>
2017-10-31 15:33:46 -0600

    hibernate
    versión 0

--- siscopiapp/src/main/java/com/siscopi/springapp/controladores/LoginCtrl.java
+++ siscopiapp/src/main/java/com/siscopi/springapp/controladores/LoginCtrl.java
@@ -8,14 +8,9 @@ package com.siscopi.springapp.controladores;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggerFactory;
 
 /**
  *
